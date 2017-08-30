95a7cd6d9d9682db09c2d1ded2adea328125f599
Tiago Gouvêa <tr617080@oi.corp.net>
2017-08-30 13:40:13 -0300

    Form and events

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -15,3 +15,29 @@ Template.body.helpers({
       return Tasks.find({});
     }
   });
+
+
+Template.body.events({ 
+    
+    'submit .new-task': function(event, template) { 
+        
+        // Prevent default browser form submit
+        event.preventDefault();
+
+        // Get value from form element
+        const target = event.target;
+        const text = target.text.value;
+
+        // Insert a task into the collection
+        Tasks.insert({
+
+            text,
+            createdAt: new Date()
+
+        });
+
+        //clear
+        target.text.value = '';
+
+    } 
+});
